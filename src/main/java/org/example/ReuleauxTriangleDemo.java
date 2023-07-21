13b7c4b90da6abb77f4dd104ad8ea5b630da8471
Your Name <you@example.com>
2023-07-21 22:38:46 +0200

    0.0.3

--- src/main/java/org/example/ReuleauxTriangleDemo.java
+++ src/main/java/org/example/ReuleauxTriangleDemo.java
@@ -5,10 +5,11 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 public class ReuleauxTriangleDemo extends JFrame {
-
     private static final int WINDOW_SIZE = 600;
+    private int innerTriangles;
 
-    public ReuleauxTriangleDemo() {
+    public ReuleauxTriangleDemo(int innerTriangles) {
+        this.innerTriangles = innerTriangles;
         setTitle("Треугольник Рёло");
         setSize(WINDOW_SIZE, WINDOW_SIZE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,13 +35,18 @@ public class ReuleauxTriangleDemo extends JFrame {
             Ellipse2D circle = new Ellipse2D.Double(cx - side / 2.0, cy - side / 2.0, side, side);
             g2d.draw(circle);
         }
-    }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new ReuleauxTriangleDemo();
-            frame.setVisible(true);
-        });
+        // Рисование внутренних треугольников
+        for (int i = 0; i < innerTriangles; i++) {
+            side -= 40;  // уменьшение размера стороны внутренних треугольников
+            height = (int) (Math.sqrt(3) * side / 2);
+            xPoints[0] = (WINDOW_SIZE - side) / 2;
+            xPoints[1] = WINDOW_SIZE / 2;
+            xPoints[2] = (WINDOW_SIZE + side) / 2;
+            yPoints[0] = yPoints[2] = (WINDOW_SIZE + height) / 2;
+            yPoints[1] = (WINDOW_SIZE - height) / 2;
+            g2d.drawPolygon(xPoints, yPoints, 3);
+        }
     }
 
 }
